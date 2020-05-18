8222829cc5f344e366bf8bd445c90d0825a9e4d6
amanpreetsingh214 <53363558+amanpreetsingh214@users.noreply.github.com>
2020-05-18 22:09:11 +0530

    Adding From MAc

---
+++ main.js
@@ -0,0 +1,12 @@
+// const ab = document.querySelector('#email');
+// const ab1 = document.querySelector('.btn-1');
+// if (ab == ''){
+//     ab1.addEventListener('submit',(e)=>{
+//         e.preventDefault();
+//         alert('Enter Your Email To Subscribe')
+//     })}
+// const ab = document.querySelector('.ab');
+document.querySelector('.ab').addEventListener("click",(e)=>{
+    e.preventDefault();
+    alert('hey');
+})
